4fa58758873d5e4a00ad8a9f59959089aa4c6090
Micah Nagel <micah.nagel@defenseunicorns.com>
2024-01-05 13:25:31 -0700

    chore: update to uds cli 0.5.2 (#64)
    * chore: update to uds cli 0.5.2
    * chore: 0.7.0 uds-core

--- terraform/vpc/templates/user-data.tpl.sh
+++ terraform/vpc/templates/user-data.tpl.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo yum install -y git libicu
-curl --retry-all-errors --retry 5 -L https://github.com/defenseunicorns/uds-cli/releases/download/v0.4.1/uds-cli_v0.4.1_Linux_amd64 -o uds
+curl --retry-all-errors --retry 5 -L https://github.com/defenseunicorns/uds-cli/releases/download/v0.5.2/uds-cli_v0.5.2_Linux_amd64 -o uds
 chmod +x uds
 sudo mv uds /usr/local/bin/
 curl --retry-all-errors --retry 5 -L https://github.com/defenseunicorns/zarf/releases/download/v0.31.4/zarf_v0.31.4_Linux_amd64 -o zarf
